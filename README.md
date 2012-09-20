f607da1a252e9aac89975e861275134232a80b24
Rufus Pollock <rufus.pollock@okfn.org>
2012-09-20 09:06:58 +0100

    [all][m]: first commit of data package for GDP including data and datapackage.json.

---
+++ README.md
@@ -0,0 +1,14 @@
+Country, regional and world GDP in current US Dollars ($). Regional means
+collections of countries e.g. Europe & Central Asia. Data is sourced from the
+World Bank and turned into a standard normalized CSV (code can be found in
+process.py of data package repository).
+
+## Source
+
+The data is sourced from the World Bank (specifically [this dataset][wb]) which
+in turn lists as sources: *World Bank national accounts data, and OECD National
+Accounts data files*.
+
+
+[wb]: data.worldbank.org/indicator/NY.GDP.MKTP.CD
+
---
+++ process.py
@@ -0,0 +1,31 @@
+import os
+import urllib
+import csv
+
+cache = 'cache'
+if not os.path.exists(cache):
+    os.makedirs(cache)
+
+dest = os.path.join(cache, 'worldbank-gdp.csv')
+urllib.urlretrieve('http://api.worldbank.org/indicator/NY.GDP.MKTP.CD?format=csv',
+        dest)
+
+fo = open(dest)
+lines = [ row for row in csv.reader(fo) ]
+headings = lines[0]
+lines = lines[1:]
+
+outheadings = [ 'Country Name', 'Country Code', 'Year', 'Value' ]
+outlines = []
+
+for row in lines:
+    for idx, year in enumerate(headings[2:]):
+        if row[idx+2]:
+            # do not convert to float as we end up with scientific notation
+            value = row[idx+2]
+            outlines.append(row[:2] + [int(year), value])
+
+writer = csv.writer(open('data/gdp.csv', 'w'))
+writer.writerow(outheadings)
+writer.writerows(outlines)
+
