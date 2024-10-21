51aed8796948ec0ea852390bfb0b6e86a16ccb50
gradedSystem <homesiknessgerald@gmail.com>
2024-10-21 15:47:23 +0700

    [update][s] Fixing up the date_time issue

--- scripts/process.py
+++ scripts/process.py
@@ -7,33 +7,33 @@ import requests
 from datetime import datetime, timedelta
 
 cache = 'cache'
-data = '../data/gdp.csv'
+data = 'data/gdp.csv'
 url = 'https://api.worldbank.org/v2/en/indicator/NY.GDP.MKTP.CD?downloadformat=excel'
 outheadings = ['Country Name', 'Country Code', 'Year', 'Value']
 current_year = datetime.now().year
-datapackage = '../datapackage.json'
+datapackage = 'datapackage.json'
 
 def create_cache():
     if not os.path.exists(cache):
         os.makedirs(cache)
 
 def create_csv(dct):
-
     rows = list(zip(dct['Country Name'], dct['Country Code'], dct['Year'], dct['Value']))
     sorted_rows = sorted(rows, key=lambda x: (x[0], x[2]))
 
-    # Write the processed data to a new CSV file
+    # Ensure the directory exists before writing
+    if not os.path.exists(os.path.dirname(data)):
+        os.makedirs(os.path.dirname(data))
+
     with open(data, 'w', newline='') as outfile:
         writer = csv.writer(outfile)
         writer.writerow(outheadings)
         writer.writerows(sorted_rows)
 
 def xldate_to_date(xldate):
-    # Excel starts on 1900-01-01 but treats 1900 as a leap year (so we subtract 1 day)
     start_date = datetime(1899, 12, 30)
     converted_date = start_date + timedelta(days=xldate)
     return converted_date.strftime('%Y-%m-%d')
-
 
 def update_datapackage(last_updated):
     with open(datapackage, 'r') as f:
@@ -49,11 +49,15 @@ def process():
     dest = os.path.join(cache, 'worldbank-gdp.xls')
     response = requests.get(url)
 
+    if response.status_code != 200:
+        print("Error downloading the file")
+        return
+
     with open(dest, 'wb') as f:
         f.write(response.content)
 
     workbook = xlrd.open_workbook(dest)
-    worksheet = workbook['Data']
+    worksheet = workbook.sheet_by_name('Data')
 
     list_rows = list(worksheet.get_rows())
 
@@ -63,12 +67,16 @@ def process():
         'Year': [],
         'Value': [],
     }
+
     last_updated = xldate_to_date(list_rows[1][1].value)
-    for values in list_rows[4:]:  
+
+    # Process all rows and remove the break
+    for values in list_rows[4:]:
         start_year = 1960
         ln = len(values[4:])
         for elem in range(ln):
             if values[4 + elem].value == '':
+                start_year += 1
                 continue
             dct['Country Name'].append(values[0].value)
             dct['Country Code'].append(values[1].value)
